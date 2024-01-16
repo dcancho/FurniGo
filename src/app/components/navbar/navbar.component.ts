e9664ad8a7b94f72f50220c96cafbaeeddee8749
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2024-01-16 12:54:18 -0500

    Code: Add the typography styles from Figma. Configure theme usage

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+		MatToolbarModule,
+		MatButtonModule
+	],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.sass'
 })
