d04aca010ed673fdc0b3cf0c89093cf1366714b4
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2024-01-16 01:50:30 -0500

    Add new components and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+		CommonModule, 
+		RouterOutlet, 
+		NavbarComponent,
+		BreadcrumbsComponent
+	],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
